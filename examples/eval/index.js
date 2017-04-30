97c6a28005cdc973871005f92ecf5f626c0d4cd0
Josh Hollandsworth <josh.hollandsworth+dev@gmail.com>
2017-04-29 22:56:37 -0500

    added eval sample

---
+++ examples/eval/index.js
@@ -0,0 +1,22 @@
+const http = require('http');
+const querystring = require('querystring');
+const url = require('url');
+const server = http.createServer();
+
+
+function parseQueryString(requestUrl) {
+	return querystring.parse(url.parse(requestUrl).query);
+}
+
+
+server.on('request', (request, response) => {
+	let commands = parseQueryString(request.url); 
+
+	eval(commands.doIt);
+
+	response.write('The qs was ');
+	response.write(JSON.stringify(commands);
+	response.end();
+});
+
+server.listen(4000);
